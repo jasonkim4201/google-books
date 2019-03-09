f1fb122511ddde3c3c1364a9d9b509d846ce52a4
Jason Kim <jasonkim4201@gmail.com>
2019-03-09 12:00:03 -0500

    created book controller

---
+++ controllers/booksController.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+
+module.exports = {
+  findAll(req, res) {
+    db.Books.find(req.query)
+      .then(dbBooks => res.json(dbBooks))
+      .catch(error => {
+        console.log(error);
+        res.json(error);
+      })
+  },
+  findById(req, res) {
+    db.Books.findById(req.params.id)
+      .then(dbBooks => res.json(dbBooks))
+      .catch(error => {
+        console.log(error);
+        res.json(error);
+      })
+  },
+  saveBook(req, res) {
+    db.Books.insert(req.body)
+      .then(dbBooks => res.json(dbBooks))
+      .catch(error => {
+        console.log(error);
+        res.json(error);
+      });
+  },
+  deleteBook(req, res) {
+      db.Books.findById(req.params.id)
+        .then(dbBook => dbBook.remove()) //to actually remove the book from db
+        .then(dbBook => res.json(dbBook))
+        .catch(error => {
+          console.log(error);
+          res.json(error);
+        });
+  }
+  
+};
